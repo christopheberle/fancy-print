9eb5e46f8e0b93be9a673bd6823515e68d6d0549
Christoph.Eberle <christoph.eberle@physik.uni-muenchen.de>
2022-11-09 13:23:45 +0100

    PendingTaskContext now uses threading

--- fancyprint/fancyprint.py
+++ fancyprint/fancyprint.py
@@ -1,7 +1,8 @@
-import time
 from enum import Enum
 import os
 import sys
+import threading
+from functools import partial
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -55,28 +56,32 @@ def print_info(msg=""):
 def print_bullet(msg):
     _print(msg, mtype=MessageType.BULLET)
 
-class PendingTaskContext():
+class PendingTaskContext():   
     def __init__(self, desc="", verbose=True):
         self.desc = desc
         self.verbose = verbose
 
     def __enter__(self):
         if self.verbose:
-            _print(self.desc, MessageType.BUSY, end="\r")
-            time.sleep(0.5)
+            threading.Thread(target=partial(_print, self.desc, MessageType.BUSY, end="\r")).start()
         return self
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
         if exc_value == None:
             if self.verbose:
-                _print(self.desc, MessageType.DONE)
+                threading.Thread(target=partial(_print, self.desc, MessageType.DONE)).start()
         else:
             if self.verbose:
-                _print(self.desc, MessageType.FAIL)
-                _print(f"ERROR: {str(exc_value)}", MessageType.FAIL)
+                threading.Thread(target=partial(self._print_error_fancy, exc_type, exc_value, exc_traceback)).start()
         return True
         
+    def _print_error_fancy(self, exc_type, exc_value, exc_traceback):
+        _print(self.desc, MessageType.FAIL)
+        _print(f"ERROR: {str(exc_value)}", MessageType.FAIL)
+        exc_traceback.print_tb()
 class NoPrint:
+    """Suppresses print statements of functions encapsulated within this context.
+    """    
     def __enter__(self):
         self._default_stdout = sys.stdout
         sys.stdout = open(os.devnull, 'w')
